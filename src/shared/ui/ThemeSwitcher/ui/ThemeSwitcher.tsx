079fbccf9274562f47379f1d60b36ceb30400f87
Kinzar97 <igor.zaripov.igor@gmail.com>
2023-02-12 17:09:08 +0300

    create ui kit button component

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import { classNames } from "shared/lib/classNames";
 import cls from "./ThemeSwitcher.module.scss";
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
+import { Button } from "shared/ui/Button";
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -12,11 +13,11 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
+    <Button
       className={classNames(cls.ThemeSwitcher, {}, [className])}
       onClick={toggleTheme}
     >
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
-    </button>
+    </Button>
   );
 };
