830042aaf749013e74be318c0c0789cb4791c712
Kinzar97 <igor.zaripov.igor@gmail.com>
2023-02-12 03:03:43 +0300

    moved routes and links to separated components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,17 @@
 import "./styles/index.scss";
-import { Routes, Route, Link } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar/>
+      <AppRouter/>
       <button onClick={toggleTheme}>Toggle theme</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
