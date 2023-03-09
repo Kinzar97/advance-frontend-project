6c1cbea42678d5fa7b136a6effd214f1ad11f9a3
Kinzar97 <igor.zaripov.igor@gmail.com>
2023-03-09 19:33:33 +0300

    add Storybook, add Button stories

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -4,6 +4,7 @@ import cls from './Button.module.scss';
 
 export enum ThemeButton {
     CLEAR = 'clear',
+    OUTLINE = 'outline'
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -13,11 +14,12 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button: FC<ButtonProps> = (props) => {
     const {
-        theme = ThemeButton.CLEAR,
+        theme,
         children,
         className,
         ...otherProps
     } = props;
+    
     return (
         <button
             type="submit"
